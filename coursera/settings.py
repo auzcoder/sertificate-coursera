a1abcb72925a1d74b3a235a823e1f656e0a2b310
Abdulkhafiz Davlatov <auz.offical@gmail.com>
2024-01-20 14:47:58 +0500

    .

--- coursera/settings.py
+++ coursera/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vo+vb1%cxp-p+%a!0l2-4uabb-kmag_pkem)qh@4dly*!s9z)+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '147.182.230.204']
 
 
 # Application definition
