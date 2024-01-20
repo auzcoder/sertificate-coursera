bbb8ac5a3d7311a0edf9f403ea8b7f9827d4c5de
Abdulkhafiz Davlatov <auz.offical@gmail.com>
2024-01-20 15:09:50 +0500

    .

--- polls/views.py
+++ polls/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+# -labaratoriya
 # def index(request):
 #     return HttpResponse("Hello, world. 3dacafbf is the polls index.")
 
 def index(request):
-    return render(request, 'frontend/core/home.html')
+    return render(request, 'index.html')
